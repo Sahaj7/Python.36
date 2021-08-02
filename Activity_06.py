03bf494ae77f4c9dc0cd32f1d9ed111e18ceeab3
Sahaj7 <88309419+Sahaj7@users.noreply.github.com>
2021-08-02 23:07:10 +0530

    reversed done commit

--- Activity_06.py
+++ Activity_06.py
@@ -1,6 +1,7 @@
 a=input("Enter 5 number")
 b=list(a)
 c=b[0:3]
+d=b[::-1]
 print("sliced list: ",c)
 b[0]=0
 b[-1]=0
@@ -8,3 +9,5 @@ c[0]=0
 c[-1]=0
 print("Replaced list1= ",b)
 print("Replaced list2= ",c)
+
+print(d)
